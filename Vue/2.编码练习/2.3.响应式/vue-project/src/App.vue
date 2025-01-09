b84257fde383d7a107a68af7d1e7661c99169bc7
wangguanran <elvans.wang@gmail.com>
2025-01-09 21:24:33 +0800

    practice for reactive

--- Vue/2.编码练习/2.3.响应式/vue-project/src/App.vue
+++ Vue/2.编码练习/2.3.响应式/vue-project/src/App.vue
@@ -1,33 +1,34 @@
 <template>
-    <div class="person">
-        <h2>姓名：{{ name }}</h2>
-        <h2>年龄：{{ age }}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">年龄+1</button>
-        <button @click="showTel">点我查看联系方式</button>
+    <div>
+        <h2>汽车信息：一台 {{ car.brand }}，价值{{ car.price }}万</h2>
+        <h2>游戏列表：</h2>
+        <ul>
+            <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+        </ul>
+        <button @click="changeCarPrice">修改汽车价格</button>
+        <button @click="changeFirstGame">修改第一游戏</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
 // 数据
-let name = ref('张三')
-let age = ref(18)
-let tel = '13888888888'
+let car = reactive({ brand: '奔驰', price: 100 })
+let games = reactive([
+    { id: '001', name: '英雄联盟' },
+    { id: "002", name: "王者荣耀" },
+    { id: '003', name: '原神' }
+])
 
 // 方法
-function changeName() {
-    name.value = 'zhangsan'
-    console.log(name)
+function changeCarPrice() {
+    car.price += 10
 }
-function changeAge() {
-    age.value += 1
-    console.log(age)
+function changeFirstGame() {
+    games[0].name = '流星蝴蝶剑'
 }
-function showTel() {
-    alert(tel)
-}
+
 
 </script>
 
