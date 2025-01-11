c01d6759d432a2313dbd7d340bde166c6764d1f6
wangguanran <elvans.wang@gmail.com>
2025-01-11 09:11:38 +0800

    practice for vue 生命周期

--- Vue/2.编码练习/2.8.生命周期/vue-project/src/App.vue
+++ Vue/2.编码练习/2.8.生命周期/vue-project/src/App.vue
@@ -1,47 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts" setup>
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue';
+
+let sum = ref(0);
+
+function changeSum() {
+    sum.value++;
+}
+console.log('App.vue setup');
+
+onBeforeMount(() => {
+    console.log('App.vue onBeforeMount');
+});
+onMounted(() => {
+    console.log('App.vue onMounted');
+});
+onBeforeUpdate(() => {
+    console.log('App.vue onBeforeUpdate');
+});
+onUpdated(() => {
+    console.log('App.vue onUpdated');
+});
+onBeforeUnmount(() => {
+    console.log('App.vue onBeforeUnmount');
+});
+onUnmounted(() => {
+    console.log('App.vue onUnmounted');
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+        <h2>当前求和为：{{ sum }}</h2>
+        <button @click="changeSum">点我sum+1</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
