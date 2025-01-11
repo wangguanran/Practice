e6c3054bfdfa839f6a3d0cdd00d94258ed8cad3f
wangguanran <elvans.wang@gmail.com>
2025-01-11 09:44:28 +0800

    practice for vue 自定义hooks

--- Vue/2.编码练习/2.9.自定义hook/vue-project/src/App.vue
+++ Vue/2.编码练习/2.9.自定义hook/vue-project/src/App.vue
@@ -1,47 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+        <h2>当前求和为：{{ sum }}，放大10倍后：{{ bigSum }}</h2>
+        <button @click="add">点我sum+1</button>
+        <hr>
+        <img v-for="(dog, index) in dogList" :src="dog" :key="index">
+        <br>
+        <button @click="getDog">再来一只小狗</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
+<script lang="ts" setup name="Person">
+import useSum from '@/hooks/useSum'
+import useDog from '@/hooks/useDog'
+
+const { sum, add, bigSum } = useSum()
+const { dogList, getDog } = useDog()
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+button {
+    margin: 0 5px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+li {
+    font-size: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+img {
+    height: 100px;
+    margin-right: 10px;
 }
 </style>
