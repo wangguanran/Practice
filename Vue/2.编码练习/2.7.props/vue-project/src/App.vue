e7e8cc0cb9802f8885687c9916ace5670bf97f86
wangguanran <elvans.wang@gmail.com>
2025-01-11 09:05:59 +0800

    practice for vue props

--- Vue/2.编码练习/2.7.props/vue-project/src/App.vue
+++ Vue/2.编码练习/2.7.props/vue-project/src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="app">
+        <Person :list="persons" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts" setup>
+import Person from './components/Person.vue'
+import { reactive } from 'vue'
+import { type Persons } from '@/types'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+let persons = reactive<Persons>([
+    { id: "1", name: 'zhangsan', age: 18 },
+    { id: "2", name: 'lisi', age: 20 },
+    { id: "3", name: 'wangwu', age: 22 },
+])
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ Vue/2.编码练习/2.7.props/vue-project/src/components/Person.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="person">
+        <ul>
+            <li v-for="item in list" :key="item.id">
+                {{ item.name }}--{{ item.age }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts" setup name="Person">
+import { defineProps } from 'vue'
+import { Persons, type PersonInter } from '@/types'
+
+// const props = defineProps(['list'])
+
+// defineProps<{ list: Persons }>()
+
+let props = withDefaults(defineProps<{ list: Persons }>(), {
+    list: () => [{ id: "1", name: 'zhangsan', age: 18 }]
+})
+
+console.log(props)
+</script>
