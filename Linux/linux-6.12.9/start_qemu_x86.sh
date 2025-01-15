143cbebd4f18ea33c5a2c226eaefee3c8a5a814c
wangguanran <elvans.wang@gmail.com>
2025-01-15 19:28:52 +0800

    update Linux/linux-6.12.9/start_qemu_x86.sh

--- Linux/linux-6.12.9/start_qemu_x86.sh
+++ Linux/linux-6.12.9/start_qemu_x86.sh
@@ -5,11 +5,16 @@ find ./drivers -name '*.ko' -exec cp {} initramfs/lib/modules/kernel/ \;
 cd initramfs
 find . | cpio -H newc -o > ../initramfs.cpio
 cd -
-# 启动内核
+# 启动内核 输出日志到控制台
 qemu-system-x86_64 \
     -kernel arch/x86/boot/bzImage \
     -initrd initramfs.cpio \
     -append "console=ttyS0,921600" \
     -serial mon:stdio \
-    -D ~/kernel.log \
     -nographic
+# 输出日志到文件中
+# qemu-system-x86_64 \
+#		-kernel arch/x86/boot/bzImage \
+#		-initrd /path/to/initramfs.cpio \
+#		-append "console=ttyS0,921600" \
+#		-serial file:kernel.log
