d354d4da381f53a6173162981ad9e8d678d14615
wangguanran <elvans.wang@gmail.com>
2025-01-16 17:09:09 +0800

    update start_qemu.sh, add --build && modify --help

--- Linux/linux-6.12.9/start_qemu.sh
+++ Linux/linux-6.12.9/start_qemu.sh
@@ -1,7 +1,30 @@
 # 解析命令行参数
-ARCH="x86_64"
+ARCH=""
 SERIAL="mon:stdio"
 DUMP_DTB=false
+BUILD_KERNEL=false
+
+print_help() {
+    echo "Usage: $0 --arch ARCH [--serial SERIAL] [--dump] [--build]"
+    echo "Options:"
+    echo "  --arch ARCH       Set the architecture (required)"
+    echo "  --serial SERIAL   Set the serial output (file or console, default: console)"
+    echo "  --dump            Dump the dtb file and convert it to dts (only for arm64)"
+    echo "  --build           Build the kernel"
+    echo "Examples:"
+    echo "  $0 --arch x86_64 --serial console --build  # Use x86_64 architecture, serial output to console, and build the kernel"
+    echo "  $0 --arch arm64 --serial file --dump       # Use arm64 architecture, serial output to file, and dump dtb file"
+    echo "  $0 --arch x86_64 --build                   # Use x86_64 architecture and build the kernel"
+    echo "  $0 --arch arm64 --serial console           # Use arm64 architecture and serial output to console"
+    echo "  $0 --arch x86_64                           # Use x86_64 architecture"
+    echo "  $0 --arch arm64 --dump                     # Use arm64 architecture and dump dtb file"
+}
+
+if [ "$#" -eq 0 ]; then
+    print_help
+    exit 0
+fi
+
 while [[ "$#" -gt 0 ]]; do
     case $1 in
     --arch)
@@ -20,14 +43,17 @@ while [[ "$#" -gt 0 ]]; do
         shift
         ;;
     --dump)
+        if [ "$ARCH" != "arm64" ]; then
+            echo "Error: --dump is only supported for arm64 architecture."
+            exit 1
+        fi
         DUMP_DTB=true
         ;;
+    --build)
+        BUILD_KERNEL=true
+        ;;
     --help)
-        echo "Usage: $0 [--arch ARCH] [--serial SERIAL] [--dump]"
-        echo "Options:"
-        echo "  --arch ARCH       Set the architecture (default: x86_64)"
-        echo "  --serial SERIAL   Set the serial output (file or console, default: console)"
-        echo "  --dump            Dump the dtb file to the root directory and convert it to dts"
+        print_help
         exit 0
         ;;
     *)
@@ -37,6 +63,12 @@ while [[ "$#" -gt 0 ]]; do
     esac
     shift
 done
+
+if [ -z "$ARCH" ]; then
+    echo "Error: --arch is a required option."
+    print_help
+    exit 1
+fi
 
 INITRAMFS_DIR="initramfs.$ARCH"
 
@@ -50,6 +82,25 @@ rm -rf $INITRAMFS_DIR/lib/modules/kernel/
 mkdir -p $INITRAMFS_DIR/lib/modules/kernel/
 # find . -name 'leds-demo.ko' -exec cp {} $INITRAMFS_DIR/lib/modules/kernel/ \;
 # find . -name 'leds-gpio.ko' -exec cp {} $INITRAMFS_DIR/lib/modules/kernel/ \;
+
+if [ "$DUMP_DTB" == true ]; then
+    BUILD_KERNEL=false # 忽略 --build 指令
+fi
+if [ "$BUILD_KERNEL" == true ]; then
+    # 编译内核
+    echo -e "\e[32mCompiling kernel...\e[0m"
+    if [ "$ARCH" == "x86_64" ]; then
+        CROMSS_COMPILER_CMD=
+        ln -sf .config.x86_64 .config
+    elif [ "$ARCH" == "arm64" ]; then
+        CROMSS_COMPILER_CMD="CROSS_COMPILE=aarch64-linux-gnu-"
+        ln -sf .config.arm64 .config
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+    make ARCH=$ARCH $CROMSS_COMPILER_CMD -j$(nproc)
+fi
 
 # 打包initramfs
 echo -e "Packing initramfs..."
